5183d5c4c87b91811d7d26a17251e477caefda44
neisha1618 <neisha1618@gmail.com>
2020-07-08 15:03:21 -0500

    added a test route for db

--- app/server/index.js
+++ app/server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('hello');
 });
@@ -12,6 +13,15 @@ app.get('/api/getList', (req, res) => {
   const list = ['item1', 'item2', 'item3'];
   res.json(list);
   console.log('Sent list of items');
+});
+
+// an endpoint to test
+app.post('/test', async(req, res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.log('problem', err);
+  }
 });
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
