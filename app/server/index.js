f03f66bc4a5fbec05c4405f17abba506b9a20afc
neisha1618 <neisha1618@gmail.com>
2020-07-08 14:31:22 -0500

    fixed lint issues

--- app/server/index.js
+++ app/server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('hello');
 });
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
+app.get('/api/getList', (req, res) => {
+  const list = ['item1', 'item2', 'item3'];
+  res.json(list);
+  console.log('Sent list of items');
 });
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });
 const port = process.env.PORT || 8000;
 app.listen(port);
-console.log('App is listening on port ' + port);
+console.log(`App is listening on port ${port}`);
