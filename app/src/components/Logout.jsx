29b47be8edb27ce161aae46bd653e8ef8257d0d3
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-09 10:08:24 -0500

    Set up dummy pages for routing testing

---
+++ app/src/components/Logout.jsx
@@ -0,0 +1,13 @@
+import  React from 'react';
+// import axios from 'axios';
+
+function Logout() {
+
+  return (
+    <div className="Logout">
+      <div>You have been logged out</div>
+    </div>
+  );
+}
+
+export default Logout;
---
+++ app/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import  React from 'react';
+// import axios from 'axios';
+
+function Profile() {
+
+  return (
+    <div className="Profile">
+      <div>Profile</div>
+    </div>
+  );
+}
+
+export default Profile;
