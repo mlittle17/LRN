9aeb1ef101d79799c473e206a6937fbb5b548fa3
neisha1618 <neisha1618@gmail.com>
2020-07-08 21:31:43 -0500

    pseudocode future methods

--- app/server/db/methods.js
+++ app/server/db/methods.js
@@ -1,2 +1,52 @@
 const pool = require('./index');
 // a change
+// database methods will go here then import into routes
+/*
+Users
+ */
+
+// method for inserting user info
+// method that gets user info
+
+/*
+Events
+ */
+// method that insert into event
+// method that get from event
+
+/*
+Topic
+ */
+// method that insert into topic
+// method that get from topix
+
+/*
+Document
+ */
+// method that insert into Document
+// method that get from Document
+
+/*
+Binder
+ */
+// method that insert into Binder
+// method that get from Binder
+
+/*
+Event_Documents
+ */
+// method that insert into Event_Documents
+// method that get from Event_Documents
+
+/*
+Flash_Cards
+ */
+// method that insert into Flash Cards
+// method that get from Flach Cards
+
+/*
+Flash card packs
+ */
+// method that insert into Flash Card Pack
+// method that get from Flash Card Pack
+
