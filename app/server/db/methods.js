0856ef2e076ecdaff62eff45a506376d9e94a1b0
neisha1618 <neisha1618@gmail.com>
2020-07-10 16:28:59 -0500

    fixed linter issues

--- app/server/db/methods.js
+++ app/server/db/methods.js
@@ -1,4 +1,4 @@
-const db = require('../db/index');
+const db = require('./index');
 // a change
 // database methods will go here then import into routes
 /*
@@ -7,44 +7,43 @@ Users
 
 // method for inserting user info
 const createUser = async(req, res) => {
-  try{
-     await db.query("INSERT INTO users (username, namefirst, namelast, email, imageurl, zip) VALUES ( ${user}, ${firstName}, ${lastName}, ${emails}, ${image}, ${zipcode})", req.body);
-    res.send({message: 'user added'})
-  } catch(err){
+  try {
+    await db.query('INSERT INTO users (username, namefirst, namelast, email, imageurl, zip) VALUES ( ${user}, ${firstName}, ${lastName}, ${emails}, ${image}, ${zipcode})', req.body);
+    res.send({ message: 'user added' });
+  } catch (err) {
     console.log('nah bruh', err);
   }
 };
 
 // method that gets  particular users info
 const getUser = async(req, res) => {
-  try{
-   let user = await db.one(`SELECT * FROM user WHERE id = ${req.params.id}`);
-   res.send(user);
-  }catch(err){
-    console.log(`no user, ${err}`)
+  try {
+    const user = await db.one(`SELECT * FROM user WHERE id = ${req.params.id}`);
+    res.send(user);
+  } catch (err) {
+    console.log(`no user, ${err}`);
   }
-}
+};
 // method that gets all the users info
 const getAllUser = async(req, res) => {
-  try{
-    let users = await db.any("SELECT * FROM users");
-    res.send(users)
-  } catch(err){
-    console.log(`no users, ${err}`)
+  try {
+    const users = await db.any('SELECT * FROM users');
+    res.send(users);
+  } catch (err) {
+    console.log(`no users, ${err}`);
   }
-}
-
+};
 
 /*
 Events
  */
 // method that insert into event
 const createEvent = async(req, res) => {
-  try{
+  try {
     // not working yet
-     await db.query("INSERT INTO event (topic, date, time, users_id, classLimit) VALUES ( ${topic}, ${date}, ${time}, ${user_Id}, ${classLimit})", req.body);
-    res.send({message: 'event added'})
-  } catch(err){
+    await db.query('INSERT INTO event (topic, date, time, users_id, classLimit) VALUES ( ${topic}, ${date}, ${time}, ${user_Id}, ${classLimit})', req.body);
+    res.send({ message: 'event added' });
+  } catch (err) {
     console.log('nah bruh', err);
   }
 };
@@ -54,11 +53,11 @@ const createEvent = async(req, res) => {
 Topic
  */
 // method that insert into topic
-const createTopic = async(req,res) => {
-  try{
-    await db.query("INSERT INTO topic (name) VALUES (${name})", req.body);
-    res.send('it worked')
-  } catch(err){
+const createTopic = async(req, res) => {
+  try {
+    await db.query('INSERT INTO topic (name) VALUES (${name})', req.body);
+    res.send('it worked');
+  } catch (err) {
     console.log('nah', err);
   }
 };
@@ -100,4 +99,4 @@ module.exports = {
   createUser,
   createTopic,
   createEvent,
-}
+};
