a9be721b689f70eae2ddb9812e046240192a45fb
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-10 15:24:48 -0500

    Build up the home/hub page with addition of the profile card and button group for routing through the rest of the applications pages

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,11 +1,31 @@
-import  React from 'react';
+import React from 'react';
 // import axios from 'axios';
+
+import ProfileCard from './ProfileCard.jsx';
+import { Grid, Typography } from '@material-ui/core';
+import { Button } from 'semantic-ui-react';
 
 function Home() {
 
   return (
     <div className="Home">
       <div>Home</div>
+      <Grid container justify="space-evenly">
+        <ProfileCard />
+        <div>
+          <>
+            <Typography gutterBottom variant="h7" component="h7"><b>SESSIONS</b></Typography>
+            <br />
+            <Button.Group size="small">
+              <Button>Create</Button>
+              <Button.Or />
+              <Button>Find</Button>
+              <Button.Or />
+              <Button>View Joined</Button>
+            </Button.Group>
+          </>
+        </div>
+      </Grid>
     </div>
   );
 }
