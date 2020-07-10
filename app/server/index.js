3e12173163a9607c9211be2648ae626ee5fdeca7
JerryMcDonald <jandh62808@gmail.com>
2020-07-10 11:41:38 -0500

    linter changes

--- app/server/index.js
+++ app/server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const app = express(); 
-const http = require("http");
+
+const app = express();
+const http = require('http');
+
 const server = http.createServer(app);
-const socket = require("socket.io");
+const socket = require('socket.io');
+
 const io = socket(server);
 const { createUser, getAllUser } = require('./db/methods');
-
-const app = express(); // create express app
 
 app.use(express.json());
 
@@ -26,59 +27,61 @@ app.get('/test', (req, res) => {
   res.send('This is from test get');
 });
 
+app.post('/user', createUser)
+
 // start express server
-  // Creating your own HTTP server to allow us the ability to reuse the server
-    // Useful for running socket.io in the same server instance
+// Creating your own HTTP server to allow us the ability to reuse the server
+// Useful for running socket.io in the same server instance
 server.listen(8000, () => console.log('server is running on port 8000'));
 
 // room id from uuid as the key
-  // an array of socket ids will be the value
-  const rooms = {};
+// an array of socket ids will be the value
+const rooms = {};
 
-  // user connects - this connection event will fire
-    // then generate a unique socket object for this individual person
-  io.on("connection", socket => {
-    // attach an event listener 
-      // Grab the room ID from the browser
-    socket.on("join room", roomID => {
-        // check if roomID exists
-        if (rooms[roomID]) {
-            // if so add the joining users socket id to the ids array
-              // (each socket will have a unique id)
-            rooms[roomID].push(socket.id);
-        } else {
-            // else create the key/value pair
-            rooms[roomID] = [socket.id];
-        }
-        // check if you are currently user a joining or user b
-        const otherUser = rooms[roomID].find(id => id !== socket.id);
-        if (otherUser) {
-            // if the other user exists in the room.. 
-              // (user b) emit a 'current' back up to ourselves with the other users socket id
-            socket.emit("other user", otherUser);
-              // emit to user a our socket.id
-            socket.to(otherUser).emit("user joined", socket.id);
-              // you can see how this plays out more on the front end. 
-        }
-    });
-  
-    // Now let us create a 'handshake'
-  
-    // when offer gets fired 
-    socket.on("offer", payload => {
-        io.to(payload.target).emit("offer", payload);
-    });
-  
-    // the payload contains who we are as a user and the 'offer' object.
-  
-    // listen for the answer event
-    socket.on("answer", payload => {
-        io.to(payload.target).emit("answer", payload);
-    });
-  
-    // what is ice canidate?
-      // each peer will come up with an 'ice server'
-    socket.on("ice-candidate", incoming => {
-        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
-    });
+// user connects - this connection event will fire
+// then generate a unique socket object for this individual person
+io.on('connection', socket => {
+  // attach an event listener
+  // Grab the room ID from the browser
+  socket.on('join room', roomID => {
+    // check if roomID exists
+    if (rooms[roomID]) {
+      // if so add the joining users socket id to the ids array
+      // (each socket will have a unique id)
+      rooms[roomID].push(socket.id);
+    } else {
+      // else create the key/value pair
+      rooms[roomID] = [socket.id];
+    }
+    // check if you are currently user a joining or user b
+    const otherUser = rooms[roomID].find(id => id !== socket.id);
+    if (otherUser) {
+      // if the other user exists in the room..
+      // (user b) emit a 'current' back up to ourselves with the other users socket id
+      socket.emit('other user', otherUser);
+      // emit to user a our socket.id
+      socket.to(otherUser).emit('user joined', socket.id);
+      // you can see how this plays out more on the front end.
+    }
+  });
+
+  // Now let us create a 'handshake'
+
+  // when offer gets fired
+  socket.on('offer', payload => {
+    io.to(payload.target).emit('offer', payload);
+  });
+
+  // the payload contains who we are as a user and the 'offer' object.
+
+  // listen for the answer event
+  socket.on('answer', payload => {
+    io.to(payload.target).emit('answer', payload);
+  });
+
+  // what is ice canidate?
+  // each peer will come up with an 'ice server'
+  socket.on('ice-candidate', incoming => {
+    io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+  });
 });
