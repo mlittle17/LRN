83cee2d0dfedd525e6e9a17399e8803ca6d796b7
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-10 15:25:24 -0500

    Add dummy Create Session fill to page for testing of second round of client side routing

---
+++ app/src/components/CreateSession.jsx
@@ -0,0 +1,13 @@
+import  React from 'react';
+// import axios from 'axios';
+
+const CreateSession = () => {
+
+  return (
+    <div className="Create">
+      <div>Create Session</div>
+    </div>
+  );
+}
+
+export default CreateSession;
