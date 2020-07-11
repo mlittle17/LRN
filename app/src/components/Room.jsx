656b1bc9742e5546271e0dd732fcf494206c6d31
JerryMcDonald <jandh62808@gmail.com>
2020-07-11 18:53:00 -0500

    start Video function

--- app/src/components/Room.jsx
+++ app/src/components/Room.jsx
@@ -17,7 +17,19 @@ const StyledVideo = styled.video`
     width: 50%;
 `;
 
+const Video = (props) => {
+  const ref = useRef();
 
+  useEffect(() => {
+    props.peer.on('stream', stream => {
+      ref.current.srcObject = stream;
+    });
+  }, []);
+
+  return (
+    <StyledVideo playsInline autoPlay ref={ref} />
+  );
+};
 
 
 
