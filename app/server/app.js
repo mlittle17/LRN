7efd63baed7c955bb9ba8cfa927a93ae61eab897
Mac <mlittle17@gmail.com>
2020-07-11 18:11:00 -0500

    add build to app.js

--- app/server/app.js
+++ app/server/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 //const cors = require('cors');
 const { routes } = require('./routes')
@@ -8,7 +9,15 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const app = express();
 app.use(express.json());
-//app.use(cors());
+
+
+// this will tell the server to use your build files
+app.use(express.static(`${path.resolve('./')}/build`));
+// this will serve files from the build when you refresh on endpoints from react router
+app.get('*', (req, res) => {
+  res.sendFile(`${path.resolve('./')}/build/index.html`);
+});
+
 app.use('/', routes);
 app.get('/', (req, res) => {
   res.send('This is from express.js');
