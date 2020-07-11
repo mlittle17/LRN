7ec5a0f88388021271bff08c54e9f1be236513b1
Mac <mlittle17@gmail.com>
2020-07-10 23:59:30 -0500

    clean up the passport file

--- app/server/config/passport-setup.js
+++ app/server/config/passport-setup.js
@@ -1,8 +1,7 @@
 const passport = require('passport');
-// const User = require();
-const User = null;
-
 const GoogleStrategy = require('passport-google-oauth2');
+const { getUser, createUser } = require('../db/methods');
+
 const keys = require('./keys');
 
 passport.serializeUser((user, done) => {
@@ -20,23 +19,24 @@ passport.use(new GoogleStrategy({
   // options for the google strategy
   callbackURL: '/auth/google/redirect',
   clientID: keys.google.clientID,
-  clientSecret: keys.google.clientSecret
+  clientSecret: keys.google.clientSecret,
 }, (accessToken, refreshToken, profile, done) => {
   // passport callback function
-  console.log('passport cb fired');
-  console.log(profile);
+  const { given_name } = profile;
+
+  const user = {};
+  user.username = given_name;
+  
   // check if user already exists in database
-  if(currentUser) {
-    console.log(currentUser, 'already here');
-    //done(null, currentUser);
+  if (false) {
+    // console.log(currentUser, 'already here');
+    // done(null, currentUser);
   } else {
     // need to call add method here to save profile
-    console.log('user is added to DB')
+    console.log('user is added to DB');
+    createUser(user);
 
     // calling done once done with new user record
-    //done(null, newUser);
+    // done(null, newUser);
   }
-
-
-})
-);
+}));
