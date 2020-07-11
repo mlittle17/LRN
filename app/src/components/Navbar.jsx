d834b7147181e8c1c53a4d5ee8d06a218086e4cf
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-10 22:07:10 -0500

    Fix linter issues. Replace the NavLink instances with Link, to remove activve link showing on selection

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
-import { NavLink, Switch, Route } from 'react-router-dom';
-import { Menu, Image } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import {
+  Link, Switch, Route,
+} from 'react-router-dom';
+import { Menu, Image } from 'semantic-ui-react';
 
 import Home from './Home.jsx';
 import Profile from './Profile.jsx';
@@ -15,58 +17,57 @@ const Navbar = () => {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-
   return (
     <div>
 
       <Menu pointing secondary>
         <Menu.Item
-          name='home'
+          name="home"
           active={activeItem === 'home'}
           onClick={handleItemClick}
         >
-          <NavLink to="/" >
-            <Image src={logo} size='mini' alt="LRN logo" />
-          </NavLink>
+          <Link to="/">
+            <Image src={logo} size="mini" alt="LRN logo" />
+          </Link>
         </Menu.Item>
 
+        <Menu.Item
+          name="sessions"
+          active={activeItem === 'sessions'}
+          onClick={handleItemClick}
+        >
+          <Link to="/profile" class="item">Profile</Link>
+        </Menu.Item>
+
+        <Menu.Item
+          name="profile"
+          active={activeItem === 'profile'}
+          onClick={handleItemClick}
+        >
+          <Link to="/sessions" class="item">Sessions</Link>
+        </Menu.Item>
+
+        <Menu.Menu position="right" class="right menu">
           <Menu.Item
-            name='sessions'
-            active={activeItem === 'sessions'}
+            name="logout"
+            active={activeItem === 'logout'}
             onClick={handleItemClick}
           >
-            <NavLink to="/profile" class="item">Profile</NavLink>
+            <Link to="/logout" class="item">Logout</Link>
           </Menu.Item>
-
-          <Menu.Item
-            name='profile'
-            active={activeItem === 'profile'}
-            onClick={handleItemClick}
-          >
-            <NavLink to="/sessions" class="item">Sessions</NavLink>
-          </Menu.Item>
-
-          <Menu.Menu position='right' class="right menu">
-            <Menu.Item
-              name='logout'
-              active={activeItem === 'logout'}
-              onClick={handleItemClick}
-            >
-              <NavLink to="/logout" class="item">Logout</NavLink>
-            </Menu.Item>
-          </Menu.Menu>
+        </Menu.Menu>
       </Menu>
 
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/sessions" component={Sessions} />
-            <Route exact path="/logout" component={Logout} />
-          </Switch>
-        </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/sessions" component={Sessions} />
+          <Route exact path="/logout" component={Logout} />
+        </Switch>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
