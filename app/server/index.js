cc6008207f4b8e5a753d9403dca55e8634975fd2
Mac <mlittle17@gmail.com>
2020-07-11 21:44:32 -0500

    add rooms to index

--- app/server/index.js
+++ app/server/index.js
@@ -2,6 +2,7 @@ const { app } = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 const socket = require('socket.io');
+
 const io = socket(server);
 // start express server
 // Creating your own HTTP server to allow us the ability to reuse the server
@@ -10,9 +11,8 @@ const port = 8080;
 server.listen(port, () => console.log(`server is running on port ${port}`));
 // room id from uuid as the key
 // an array of socket ids will be the value
-const rooms = {};
-// user connects - this connection event will fire
-// then generate a unique socket object for this individual person
+const users = {};
+const socketToRoom = {};
 io.on('connection', socket => {
   socket.on('join room', roomID => {
     if (users[roomID]) {
@@ -27,18 +27,14 @@ io.on('connection', socket => {
     }
     socketToRoom[socket.id] = roomID;
     const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
     socket.emit('all users', usersInThisRoom);
   });
-
   socket.on('sending signal', payload => {
     io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
   });
-
   socket.on('returning signal', payload => {
     io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
   });
-
   socket.on('disconnect', () => {
     const roomID = socketToRoom[socket.id];
     let room = users[roomID];
@@ -48,4 +44,3 @@ io.on('connection', socket => {
     }
   });
 });
-
