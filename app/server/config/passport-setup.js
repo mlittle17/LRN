36c9d014e50e9fcc13b0cc13f5d36b252f87666b
Mac <mlittle17@gmail.com>
2020-07-12 19:01:46 -0500

    complete the passport file

--- app/server/config/passport-setup.js
+++ app/server/config/passport-setup.js
@@ -6,13 +6,18 @@ const keys = require('./keys');
 
 passport.serializeUser((user, done) => {
   // calling done method once we get the user from the db
-  // done(null, user.id)
+  const me = user;
+  done(null, user.googleid);
 });
 
 passport.deserializeUser((id, done) => {
   // meed to find user by id
   // calling once we've found the user
-  done(null, user.id);
+  getUser(id)
+    .then(currentUser => {
+      currentUser[0];
+      done(null, currentUser[0]);
+    });
 });
 
 passport.use(new GoogleStrategy({
@@ -22,20 +27,37 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.google.clientSecret,
 }, (accessToken, refreshToken, profile, done) => {
   // passport callback function
+  const {
+    id: googleId, displayName: username, given_name: firstName, family_name: lastName, picture: photo,
+  } = profile;
 
-  getUser('me@me.com')
-    .then(res => console.log(res));
+  const user = {
+    googleId,
+    username,
+    firstName,
+    lastName,
+    photo,
 
-  // check if user already exists in database
+  };
 
-  // if (false) {
-  //   // console.log(currentUser, 'already here');
-  //   // done(null, currentUser);
-  // } else {
-    // need to call add method here to save profile
-    //createUser(profile);
-
-    // calling done once done with new user record
-    //done(null, newUser);
-  }
-));
+  getUser(googleId)
+    .then(currentUser => {
+      currentUser;
+      // if the response includes a user object from our databse
+      if (currentUser.length) {
+        console.log('hello');
+        console.log(currentUser[0]);
+        done(null, currentUser[0]);
+      } else {
+      // if not, create a new user in the database
+        createUser(user);
+        console.log('this is a test');
+        getUser(googleId)
+          .then(newUser => {
+            newUser;
+            done(null, newUser[0]);
+          })
+          .catch(err => console.log(err));
+      }
+    });
+}));
