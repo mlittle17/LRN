b2581b3713d22dd5fc86f43176ee1692568bd186
neisha1618 <neisha1618@gmail.com>
2020-07-12 19:07:35 -0500

    Profile page will render Binder component

--- app/src/components/Profile.jsx
+++ app/src/components/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Binder from './Binder';
 // import axios from 'axios';
 
 function Profile() {
   return (
     <div className="Profile">
       <div>Profile</div>
+      <Binder />
     </div>
   );
 }
