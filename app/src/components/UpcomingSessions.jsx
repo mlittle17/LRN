8b3cca1821bf1de791a17253b4bc839ed9ff0bf6
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-13 07:47:10 -0500

    Create custom chunk method for breaking the array of session upcoming into individual page collections, create a new table component for each slide and allow the user to travel back and forth between pages

--- app/src/components/UpcomingSessions.jsx
+++ app/src/components/UpcomingSessions.jsx
@@ -3,23 +3,45 @@ import React, { useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
 import Slider from 'react-slick';
 
+import UpcomingTable from './UpcomingTable.jsx';
 import '../styles/Upcoming.css';
 
 const sessions = [
   {
-    creator: 'fdfdff',
-    date: '09/34/7899',
-    time: '05:34 pm',
+    name: 'History of the Aztec',
+    creator: 'Prof. Alan Thicke',
+    date: '07/26/2020',
+    time: '5:30 pm',
   },
   {
-    creator: 'ryknnl',
-    date: '05/67/1253',
-    time: '12:25 am',
+    name: 'Homestyle Lasagna: The True Italian Way',
+    creator: 'Rya Sicily',
+    date: '08/03/2020',
+    time: '7:25 pm',
   },
   {
-    creator: 'dsdf',
-    date: "14/04/4565",
-    time: '56:54 pm pm',
+    name: 'Traveling Light',
+    creator: 'Greg Sanzen',
+    date: '08/10/2020',
+    time: '11:00 am',
+  },
+  {
+    name: 'One, Two, Rhythm For Beginners',
+    creator: 'Sarah Courtz',
+    date: '09/15/2020',
+    time: '6:00 pm',
+  },
+  {
+    name: 'Basics of Astronomy: Stars',
+    creator: 'Rita Teller',
+    date: '09/17/2020',
+    time: '8:00 pm',
+  },
+  {
+    name: 'Balancing Your Checkbook',
+    creator: 'Yare Rewwn',
+    date: '10/10/2020',
+    time: '12:00 pm',
   },
 ];
 const settings = {
@@ -31,19 +53,34 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
 };
+
+// eslint-disable-next-line no-extend-native
+Object.defineProperty(Array.prototype, 'chunk', {
+  // eslint-disable-next-line func-names
+  // eslint-disable-next-line object-shorthand
+  value: function (chunkSize) {
+    const P = [];
+    for (let i = 0; i < this.length; i += chunkSize) {
+      P.push(this.slice(i, i + chunkSize));
+    }
+    return P;
+  },
+});
+
 const UpcomingSessions = () => {
   // useEffect(() => {
 
   // }, []);
 
+  const rows = sessions.chunk(3);
   return (
     <div>
       <Typography gutterBottom variant="h6" component="h6"><b>UPCOMING</b></Typography>
-      <Slider {...settings} style={{ width: '400px', height: '150px', backgroundColor: '#e0e1e2' }}>
-        {sessions.map((page) => {
+      <Slider {...settings} style={{ width: '600px', height: '300px', backgroundColor: '#e0e1e2' }}>
+        {rows.map((sessionPage) => {
           return (
             <div>
-              {page.creator}
+              <UpcomingTable sessionPage={sessionPage} />
             </div>
           );
         })}
