aec4c0bb4377638a1206519aa7ff922aca81c4df
neisha1618 <neisha1618@gmail.com>
2020-07-12 21:05:34 -0500

    styled the binder table

--- app/src/components/Binder.jsx
+++ app/src/components/Binder.jsx
@@ -1,16 +1,112 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Button, Card, CardContent, CardActionArea, CardActions, Grid, Typography, IconButton,
+  Avatar, Collapse, CardHeader, Table, TableBody, TableRow, TableCell, TableContainer, TableHead,
+} from '@material-ui/core';
 import axios from 'axios';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(() => ({
+  table: {
+    maxWidth: 150,
+    minWidth: 100,
+  },
+}));
+
+/*
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function SimpleTable() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+ */
 
 function Binder() {
+  // const[]
+  const classes = useStyles();
   return (
     <div className="Binder">
       <h1>My Binder</h1>
-      <tbody>
-        <td>Type</td>
-        <td> Name</td>
-        <td> Creator</td>
-        <td> Date Created</td>
-      </tbody>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell size="small" align="right">Type</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Creator</TableCell>
+              <TableCell align="right">Created Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell component="th" scope="row">
+              </TableCell>
+              <TableCell align="right">will be data</TableCell>
+              <TableCell align="right">will be data</TableCell>
+              <TableCell align="right">will be data</TableCell>
+              <TableCell align="right">will be data</TableCell>
+            </TableRow>
+            {/* ))} */}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
