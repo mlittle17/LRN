5d012f6aafbfd7d5b8179288cfd2bdf6aa2328b0
JerryMcDonald <jandh62808@gmail.com>
2020-07-13 00:02:47 -0500

    material-ui

--- app/src/components/Profile.jsx
+++ app/src/components/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ProfileCard from './ProfileCard';
 import Binder from './Binder';
+import {Grid} from '@material-ui/core';
 // import axios from 'axios';
 
 function Profile() {
   return (
     <div className="Profile">
+      <Grid container justify="space-around">
       <ProfileCard />
       <Binder />
+      </Grid>
     </div>
   );
 }
