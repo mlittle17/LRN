8f79f1a7e21fc5a6b2a51bb8f7d8dbb339a5db52
JerryMcDonald <jandh62808@gmail.com>
2020-07-13 21:03:59 -0500

    remove test route and add dotenv

--- app/server/app.js
+++ app/server/app.js
@@ -1,21 +1,21 @@
-
 const express = require('express');
-//const cors = require('cors');
-const { routes } = require('./routes')
-const authRoutes = require('./auth-routes');
-const passportSetup = require('./config/passport-setup');
+// const cors = require('cors');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const { routes } = require('./routes');
+const authRoutes = require('./auth-routes');
+const passportSetup = require('./config/passport-setup');
 const keys = require('./config/keys');
+
 const app = express();
 const path = require('path');
-app.use(express.json());
+require('dotenv').config();
 
+app.use(express.json());
 
 // this will tell the server to use your build files
 app.use(express.static(`${path.resolve('./')}/build`));
 // // this will serve files from the build when you refresh on endpoints from react router
-
 
 app.use('/', routes);
 app.get('/', (req, res) => {
@@ -32,18 +32,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 // auth route
 app.use('/auth', authRoutes);
-app.post('/test', async(req, res) => {
-  try {
-    console.log('i am hitting post test');
-    res.send('This is from test route');
-  } catch (err) {
-    console.log('problem', err);
-  }
-});
 
 app.get('*', (req, res) => {
   res.sendFile(`${path.resolve('./')}/build/index.html`);
 });
 module.exports = {
   app,
-}
+};
