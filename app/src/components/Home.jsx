cfb4ba4479e65d5595142c92c8e756e22fd4f567
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-14 15:02:37 -0500

    Break up the button group set and space individually in grid. Recolor and style to fit design scheme. Rearrange to match the order and wording of wireframe

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -6,11 +6,11 @@ import { Grid, Typography } from '@material-ui/core';
 import { Button } from 'semantic-ui-react';
 
 import ProfileCard from './ProfileCard.jsx';
-// import CreateSession from './CreateSession.jsx';
 import FindSessions from './FindSessions.jsx';
-// import Sessions from './Sessions.jsx';
 import Binder from './Binder.jsx';
 import UpcomingSessions from './UpcomingSessions.jsx';
+
+import '../styles/Home.css';
 
 const Home = () => {
   return (
@@ -18,27 +18,28 @@ const Home = () => {
       <Grid container justify="space-evenly" style={{ marginBottom: '30px' }}>
         <ProfileCard />
         <div>
+          {/* <Typography gutterBottom variant="h7" component="h7"><b>SESSIONS</b></Typography> */}
           <>
-            <Typography gutterBottom variant="h7" component="h7"><b>SESSIONS</b></Typography>
-            <br />
-            <Button.Group size="small">
-              <Button as={Link} to="/create">Create</Button>
-              <Button.Or />
-              <Button as={Link} to="/find">Find</Button>
-              <Button.Or />
-              <Button as={Link} to="/registered">View Joined</Button>
-            </Button.Group>
+            {/* <br /> */}
+            <Button as={Link} to="/registered" style={{ backgroundColor: '#474a2c', color: '#f6fef5' }}>My Sessions</Button>
+
+            <Button as={Link} to="/find" style={{ backgroundColor: '#474a2c', color: '#f6fef5' }}>Find Sessions</Button>
+
+            <Button as={Link} to="/create" style={{ backgroundColor: '#474a2c', color: '#f6fef5' }}>Create Session</Button>
           </>
-          <UpcomingSessions />
+          <div style={{ marginTop: '30px' }}>
+            <UpcomingSessions />
+          </div>
         </div>
       </Grid>
-      <Binder />
+      <div>
+        <Binder />
+      </div>
 
       <div>
         <Switch>
-          {/* <Route exact path="/create" component={CreateSession} /> */}
           <Route exact path="/find" component={FindSessions} />
-          {/* <Route exact path="/registered" component={Sessions} /> */}
+
         </Switch>
       </div>
     </div>
