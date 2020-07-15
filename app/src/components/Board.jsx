8f57af8d3405ab01303e059c873c8f55635f751e
JerryMcDonald <jandh62808@gmail.com>
2020-07-15 10:01:14 -0500

    throttle down the events per second

--- app/src/components/Board.jsx
+++ app/src/components/Board.jsx
@@ -76,7 +76,18 @@ const Board = () => {
       current.y = e.clientY || e.touches[0].clientY;
     };
 
+    // limit the number of events per second
+    const throttle = (callback, delay) => {
+      let previousCall = new Date().getTime();
+      return function() {
+        const time = new Date().getTime();
 
+        if ((time - previousCall) >= delay) {
+          previousCall = time;
+          callback.apply(null, arguments);
+        }
+      };
+    };
 
 
 
