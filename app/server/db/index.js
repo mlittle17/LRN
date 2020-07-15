bf09774b7034ba25bb097f6ad9a4f3e582f7ef49
Mac <mlittle17@gmail.com>
2020-07-15 17:21:09 -0500

    destructure env variables

--- app/server/db/index.js
+++ app/server/db/index.js
@@ -1,25 +1,19 @@
 const pgp = require('pg-promise')({});
-require('dotenv').config()
+require('dotenv').config();
 
-
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-// const DB_HOST = process.env.DB_HOST;
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  DB_HOST,
+} = process.env;
 
 const db = pgp({
   user: DB_USER,
   password: DB_PASS,
-  host: 'localhost',
-  //host: `/cloudsql/${process.env.DB_INSTANCE}`,
+  host: DB_HOST,
   port: 5432,
   database: DB_NAME,
-
-  // user: DB_USER,
-  // password: DB_PASS,
-  // host: 'localhost',
-  // port: 5432,
-  // database: 'lrn',
 
 });
 
