93b94998d8aa4e08dbc39b008b2a18158c827ce0
neisha1618 <neisha1618@gmail.com>
2020-07-16 22:06:46 -0500

    fixed a event route

--- app/server/routes/event.js
+++ app/server/routes/event.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { createEvent, getAllEvents, getEventbyUser, addDocument, getEventDocument } = require('../db/methods');
+const { createEvent, getAllEvents, getEventbyUser, addDocument, getEventDocument, getAllDocument } = require('../db/methods');
 
 const eventRouter = Router();
 
-eventRouter.post('/', createEvent, addDocument);
+eventRouter.post('/', createEvent);
 eventRouter.get('/', getAllEvents);
 eventRouter.post('/documents', addDocument);
+eventRouter.get('/documents', getAllDocument);
 eventRouter.get('/:id/documents', getEventDocument);
 eventRouter.get('/:id', getEventbyUser);
 
