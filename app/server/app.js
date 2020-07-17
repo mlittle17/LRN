cb43eabba8e3700dc929c7b89e47510a0daac4f7
Mac <mlittle17@gmail.com>
2020-07-17 15:19:29 -0500

    add https Redirect to app file

--- app/server/app.js
+++ app/server/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-// const cors = require('cors');
+const httpsRedirect = require('express-https-redirect');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const cors = require('cors');
 const { routes } = require('./routes');
 const authRoutes = require('./auth-routes');
 const passportSetup = require('./config/passport-setup');
-const keys = require('./config/keys');
 
 const app = express();
 const path = require('path');
@@ -17,7 +16,7 @@ app.use(cors());
 // this will tell the server to use your build files
 app.use(express.static(`${path.resolve('./')}/build`));
 // // this will serve files from the build when you refresh on endpoints from react router
-
+app.use('/', httpsRedirect());
 app.use('/', routes);
 app.get('/', (req, res) => {
   res.send('This is from express.js');
