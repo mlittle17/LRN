8cc243dd96616a9c9e4a7380f8a61d71dce5d4c8
JerryMcDonald <jandh62808@gmail.com>
2020-07-16 19:59:37 -0500

    bring in board two

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -13,6 +13,7 @@ import CreateSession from './CreateSession.jsx';
 import CreateRoom from '../routes/CreateRoom';
 import Room from './Room.jsx';
 import Board from './Board.jsx';
+import BoardTwo from './BoardTwo.jsx'
 import Canvas from './Canvas.jsx';
 
 import 'semantic-ui-css/semantic.min.css';
@@ -67,6 +68,23 @@ const Navbar = ({ user, googleLogin }) => {
             <Link to="/profile" class="item" style={{ color: '#a58e57', fontSize: '24px' }}>Profile</Link>
           </Menu.Item>
 
+          <Menu.Item
+          name="board"
+          active={activeItem === 'board'}
+          onClick={handleItemClick}
+        >
+          <Link to="/board" class="item" style={{ color: '#a58e57', fontSize: '24px' }}>Board</Link>
+        </Menu.Item>
+
+        {/* <Menu.Item
+          name="boardTwo"
+          active={activeItem === 'boardTwo'}
+          onClick={handleItemClick}
+        >
+          <Link to="/boardTwo" class="item" style={{ color: '#a58e57', fontSize: '24px' }}>BoardTwo</Link>
+        </Menu.Item> */}
+
+
           {/* <Menu.Menu position="right" class="right menu">
         <Menu.Item
           name="profile"
@@ -81,14 +99,6 @@ const Navbar = ({ user, googleLogin }) => {
           onClick={handleItemClick}
         >
           <Link to={`/room/${id}`} class="item">Room</Link>
-        </Menu.Item>
-
-        <Menu.Item
-          name="board"
-          active={activeItem === 'board'}
-          onClick={handleItemClick}
-        >
-          <Link to="/board" class="item">Whiteboard</Link>
         </Menu.Item>
 
         <Menu.Item
@@ -131,7 +141,8 @@ const Navbar = ({ user, googleLogin }) => {
           <Route exact path="/create" component={CreateSession} />
           <Route exact path="/registered" component={Sessions} />
           <Route path="/room/:roomID" component={Room} />
-          <Route exact path="/board" component={Board} />
+          <Route exact path="/board" render={() => (<Board />)} />
+          <Route exact path="/boardTwo" render={() => (<BoardTwo />)} />
           <Route exact path="/canvas" component={Canvas} />
           <Route exact path="/create" component={CreateSession} />
         </Switch>
