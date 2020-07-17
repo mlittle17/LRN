a8dff8d7d07eaffcb1492e1504218d88023d2d86
Mac <mlittle17@gmail.com>
2020-07-17 00:01:42 -0500

    add BoardTwo to project

---
+++ app/src/components/BoardTwo.jsx
@@ -0,0 +1,16 @@
+import React, { useRef, useEffect } from 'react';
+import io from 'socket.io-client';
+import { Container } from 'semantic-ui-react';
+import CanvasDraw, {getSaveData} from "react-canvas-draw";
+const BoardTwo = () => {
+  const socketRef = useRef();
+const onDrawingEvent = (data) => {
+    console.log('hello');
+  }
+  return (
+    <Container>
+      <CanvasDraw onChange={() => onDrawingEvent()} />
+    </Container>
+  );
+};
+export default BoardTwo;
