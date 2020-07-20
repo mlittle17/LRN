ea13328794874bed9d4b4da0c9331f9bbfdb3f44
neisha1618 <neisha1618@gmail.com>
2020-07-20 15:21:27 -0500

    reassign document to binder

--- app/src/components/Profile.jsx
+++ app/src/components/Profile.jsx
@@ -6,14 +6,14 @@ import ProfileCard from './ProfileCard.jsx';
 import Binder from './Binder.jsx';
 // import axios from 'axios';
 
-function Profile({ user, documents }) {
+function Profile({ user, binder }) {
   // console.log(user.username, 'inside profile component');
 
   return (
     <div className="Profile">
       <Grid container justify="space-around">
         <ProfileCard userInfo={user} />
-        <Binder userInfo={user} documents={documents} />
+        <Binder userInfo={user} binder={binder} />
       </Grid>
     </div>
   );
