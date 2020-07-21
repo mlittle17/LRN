842aab59c0f691212f2a6cdb20def1076ebf3a6f
JerryMcDonald <jandh62808@gmail.com>
2020-07-21 09:31:45 -0500

    Session Router

---
+++ app/src/components/SessionRoute.jsx
@@ -0,0 +1,18 @@
+import React, {
+  useState, MouseEvent, KeyboardEvent, useEffect, useRef,
+} from 'react';
+
+const SessionRoute = ({ user }) => {
+  const [joinCount, setJoinCount] => {0};
+
+  const userJoins = () => {
+    setJoinCount(joinCount + 1);
+  }
+
+  return (
+
+    <div>
+
+    </div>
+  );
+};
