af9c1defbf5a209133a7def729b98a28d3b6f004
JerryMcDonald <jandh62808@gmail.com>
2020-07-21 22:30:33 -0500

    bring in connections

--- app/src/components/InstructorSession.jsx
+++ app/src/components/InstructorSession.jsx
@@ -1,37 +1,122 @@
 import React, { useRef, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import Peer from 'simple-peer';
 
-const StyledVideo = styled.video`
-position: static;
-border: 1px solid #cddfe7;
-bottom: 60px;
-right: 40px;
-box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
-border-radius: 5px;
-width: 300px;
-`;
+// the video style
+import StyledVideo from '../styles/StyledComponents.js';
+
+import Slider from 'react-slick';
+import { Grid, Typography, Avatar } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Board from './Board.jsx';
+// import Video from './Video.jsx';
+import Questions from './Questions.jsx';
+import socket from './Socket.jsx';
+
+import '../styles/Upcoming.css';
+
 
 const videoConstraints = {
   height: window.innerHeight / 2,
   width: window.innerWidth / 2,
 };
 
+
 const InstructorSession = (props) => {
+  const [peers, setPeers] = useState([]);
+  // const socket = useRef();
   const userVideo = useRef();
+  const peersRef = useRef([]);
+  const { roomID } = props.match.params;
+  const [videoSwitch, setVideoSwitch] = useState(true);
+  const [audioSwitch, setAudioSwitch] = useState(true);
 
 
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
       userVideo.current.srcObject = stream;
-  });
+      socket.current.emit('join room', roomID);
+      socket.current.on('all users', users => {
+        const peers = [];
+        users.forEach(userID => {
+          const peer = createPeer(userID, socket.current.id, stream);
+          peersRef.current.push({
+            peerID: userID,
+            peer,
+          });
+          peers.push(peer);
+        });
+        setPeers(peers);
+      });
+
+      socket.current.on('user joined', payload => {
+        const peer = addPeer(payload.signal, payload.callerID, stream);
+        peersRef.current.push({
+          peerID: payload.callerID,
+          peer,
+        });
+
+        setPeers(users => [...users, peer]);
+      });
+
+      socket.current.on('receiving returned signal', payload => {
+        const item = peersRef.current.find(p => p.peerID === payload.id);
+        item.peer.signal(payload.signal);
+      });
+    });
   }, []);
+
+  const createPeer = (userToSignal, callerID, stream) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    });
+
+    peer.on('signal', signal => {
+      socket.current.emit('sending signal', { userToSignal, callerID, signal });
+    });
+
+    return peer;
+  }
+
+  const addPeer = (incomingSignal, callerID, stream) => {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+
+    peer.on('signal', signal => {
+      socket.current.emit('returning signal', { signal, callerID });
+    });
+
+    peer.signal(incomingSignal);
+
+    return peer;
+  }
+
+  const pauseVideo = () => {
+    if (userVideo.current.srcObject) {
+      console.log('pause video button', !videoSwitch, userVideo.current.srcObject.getVideoTracks());
+      userVideo.current.srcObject.getVideoTracks()[0].enabled = !videoSwitch;
+      setVideoSwitch(!videoSwitch);
+    }
+  }
+
+  const mute = () => {
+    if (userVideo.current.srcObject) {
+      // userVideo.current.srcObject.getAudioTracks()[0].enabled = !audioSwitch;
+      setAudioSwitch(!audioSwitch);
+    }
+  }
 
 
   return (
     <div>
-      ttt
+      
       <a>This is the instructors page</a>
+      <h1> Event Name </h1>
       {props.vid}
       {/* <StyledVideo playsInline autoPlay ref={ref} /> */}
       <StyledVideo muted ref={userVideo} autoPlay playsInline />
