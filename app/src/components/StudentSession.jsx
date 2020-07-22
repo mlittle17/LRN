7173d33b106a8362d3f21af4df1a73a948ab7561
JerryMcDonald <jandh62808@gmail.com>
2020-07-21 22:06:58 -0500

    start student session page

---
+++ app/src/components/StudentSession.jsx
@@ -0,0 +1,9 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+const StudentSession = () => {
+  return (
+    <a>This is the Students page</a>
+  );
+};
+
+export default StudentSession;
