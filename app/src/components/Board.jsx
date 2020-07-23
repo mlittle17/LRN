7caff3482b3750cd2706797b12e076acb8d92726
JerryMcDonald <jandh62808@gmail.com>
2020-07-23 02:17:21 -0500

    context.fillRect

--- app/src/components/Board.jsx
+++ app/src/components/Board.jsx
@@ -33,6 +33,7 @@ const Board = () => {
     const test = colorsRef.current;
     const context = canvas.getContext('2d');
 
+    context.fillRect(0,0,canvas.width,canvas.height);
     // ---------------------------------------------------------------------------------
 
     // ----------------------- Colors --------------------------------------------------
