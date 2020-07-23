d4e6b9068fc236e713c1ef361ef19c8db6c6814c
neisha1618 <neisha1618@gmail.com>
2020-07-23 01:31:55 -0500

    removed icons form create session

--- app/src/components/CreateSession.jsx
+++ app/src/components/CreateSession.jsx
@@ -8,10 +8,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardContent, Grid } from '@material-ui/core';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import Brightness3Icon from '@material-ui/icons/Brightness3';
-import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile'; // for document
-import SlideshowTwoToneIcon from '@material-ui/icons/SlideshowTwoTone'; // two-toned for pp
-import ViewCarouselTwoToneIcon from '@material-ui/icons/ViewCarouselTwoTone'; // flash cards
-
 import AddDocuments from './AddDocuments.jsx';
 import '../styles/Form.css';
 
@@ -57,10 +53,6 @@ const CreateSession = ({ user }) => {
   const [capacity, setCapacity] = useState(1);
   const [subject, setSubject] = useState('');
   const [document, setDocument] = useState('');
-  // const [eventId, setEventId] = useState(1);
-
-  // for now hardcoded user
-  // const user_id = '1'
 
   const onSessionDateChange = (e) => {
     setSessionDate(e.target.rawValue);
