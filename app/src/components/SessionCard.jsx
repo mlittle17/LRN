d15419451359f38db542ecf0bbd3cf4b1e6e38ff
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-07-24 03:59:11 -0500

    Add open and close functions to allow the card to shown or hidden upon select or exit. Add a dialog around the card component that will give the effect of the card appearing in front of the screen, modal style. Create a button that will show on the calendar to allow click open of the session card. Toggle the visibility of the register option of a button, dependent on whether the card is on the my sessions page or the sessions page

--- app/src/components/SessionCard.jsx
+++ app/src/components/SessionCard.jsx
@@ -6,7 +6,7 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Avatar, Button, Card, CardContent, CardActionArea, CardActions,
-  CardHeader, Collapse, Grid, Typography, IconButton,
+  CardHeader, Collapse, Dialog, Grid, Typography, IconButton,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
@@ -15,6 +15,12 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 300,
     minWidth: 300,
     borderColor: '#474a2c',
+  },
+  dialog: {
+    maxWidth: 364,
+    minWidth: 364,
+    minHeight: 550,
+    marginLeft: 'auto',
   },
   actionArea: {
     backgroundColor: '#a58e57',
@@ -33,9 +39,21 @@ const useStyles = makeStyles((theme) => ({
 
 const SessionCard = ({ event }) => {
   const classes = useStyles();
-  const [cardOpen, setCardOpen] = useState('false');
+  const [open, setOpen] = useState(false);
   const [expanded, setExpanded] = React.useState(false);
 
+  // Open the event card window
+  const handleClickOpen = () => {
+    console.log(event);
+    setOpen(true);
+  };
+
+  // Close the event card window
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Set the Card to expand show the extra details
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
@@ -43,110 +61,130 @@ const SessionCard = ({ event }) => {
   const location = useLocation();
 
   return (
-    <Card className={classes.root} variant="outlined">
+    <>
+      <Button size="small" onClick={handleClickOpen}>
+        SESSION {/* {event.name} */}
+      </Button>
+      <Dialog
+        fullWidth="true"
+        className={classes.dialog}
+        maxWidth="md"
+        scroll="paper"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="max-width-dialog-title"
+        hideBackdrop="true"
+      >
+        {/* {/* {event && ( */}
+          <Card className={classes.root} variant="outlined">
+            <CardActionArea className={classes.actionArea}>
+              <div style={{ marginTop: '10px' }}>
+                <Typography gutterBottom variant="h4" component="h1" align="center">
+                  {/* {event.name} */}
+                </Typography>
+              </div>
+              <br />
+            </CardActionArea>
+            <CardContent>
+              <div>
+                {/* Possibly add an avatar here with instructor name */}
+                <Typography gutterBottom variant="h5" component="h2">
+                  Instructor
+            </Typography>
+                {/* Possibly add an avatar here with instructor name */}
+                <Typography variant="body1" color="textSecondary" component="p">
+                  {/* {`${event.namefirst} ${event.namelast}`} */}
+                </Typography>
+              </div>
+              <br />
+              <Grid container justify="space-between">
+                <div>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Subject
+              </Typography>
+                  <Typography variant="body1" color="textSecondary" component="p">
+                    {/* {event.topic} */}
+                  </Typography>
+                </div>
+                <div>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Capacity
+              </Typography>
+                  <Typography variant="body1" color="textSecondary" component="p">
+                    {/* {event.capacityCount} */}
+                  </Typography>
+                </div>
+              </Grid>
+              <br />
+              <Grid container justify="space-between">
+                <div>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Date
+              </Typography>
+                  <Typography variant="body1" color="textSecondary" component="p">
+                    {/* {event.date} */}
+                  </Typography>
+                </div>
+                <div>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Time
+              </Typography>
+                  <Typography variant="body1" color="textSecondary" component="p">
+                    {/* {event.time} */}
+                  </Typography>
+                </div>
+              </Grid>
 
-      <CardActionArea className={classes.actionArea}>
-        <div style={{ marginTop: '10px' }}>
-          <Typography gutterBottom variant="h4" component="h1" align="center">
-            {/* {event.name} */}
-          </Typography>
-        </div>
-        <br />
-      </CardActionArea>
-      <CardContent>
-        <div>
-          {/* Possibly add an avatar here with instructor name */}
-          <Typography gutterBottom variant="h5" component="h2">
-            Instructor
-          </Typography>
-          {/* Possibly add an avatar here with instructor name */}
-          <Typography variant="body1" color="textSecondary" component="p">
-            {/* {`${event.namefirst} ${event.namelast}`} */}
-          </Typography>
-        </div>
-        <br />
-        <Grid container justify="space-between">
-          <div>
-            <Typography gutterBottom variant="h5" component="h2">
-              Subject
-            </Typography>
-            <Typography variant="body1" color="textSecondary" component="p">
-              {/* {event.topic} */}
-            </Typography>
-          </div>
-          <div>
-            <Typography gutterBottom variant="h5" component="h2">
-              Capacity
-            </Typography>
-            <Typography variant="body1" color="textSecondary" component="p">
-              {/* {event.capacityCount} */}
-            </Typography>
-          </div>
-        </Grid>
-        <br />
-        <Grid container justify="space-between">
-          <div>
-            <Typography gutterBottom variant="h5" component="h2">
-              Date
-            </Typography>
-            <Typography variant="body1" color="textSecondary" component="p">
-              {/* {event.date} */}
-            </Typography>
-          </div>
-          <div>
-            <Typography gutterBottom variant="h5" component="h2">
-              Time
-            </Typography>
-            <Typography variant="body1" color="textSecondary" component="p">
-              {/* {event.time} */}
-            </Typography>
-          </div>
-        </Grid>
+            </CardContent>
+            <CardActions>
+              <Grid container justify="space-between">
+                <div>
+                  <Button size="small" onClick={handleClose} style={{ color: '#f7fff6', backgroundColor: '#474a2c' }}>
+                    Close
+              </Button>
+                  {/* && registered !== true */}
+                  {(location.pathname !== '/registered') && (
+                    <Button size="small" style={{ color: '#f7fff6', backgroundColor: '#474a2c' }}>
+                      Register
+                    </Button>
+                  )}
+                </div>
+                <IconButton
+                  className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                  })}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </IconButton>
+              </Grid>
+            </CardActions>
 
-      </CardContent>
-      <CardActions>
-        <Button size="small" style={{ color: '#f7fff6', backgroundColor: '#474a2c' }}>
-          Close
-        </Button>
-        {/* && registered !== true */}
-        { (location.pathname !== '/registered') && (
-          <Button size="small" style={{ color: '#f7fff6', backgroundColor: '#474a2c' }}>
-            Register
-          </Button>
-        )}
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Description:
+            </Typography>
+                <Typography variant="body1" color="textSecondary" component="p">
+                  What was the class structure of the Aztecs? What language did the Aztec speak?
+                  What can we learn from the Aztecs? Find out the answers to these questions and
+                  more in this comprehensive session pulled straight from my course curriculum at Tulane.
+            </Typography>
 
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Description:
-          </Typography>
-          <Typography variant="body1" color="textSecondary" component="p">
-            What was the class structure of the Aztecs? What language did the Aztec speak?
-            What can we learn from the Aztecs? Find out the answers to these questions and
-            more in this comprehensive session pulled straight from my course curriculum at Tulane.
-          </Typography>
-
-          <Typography gutterBottom variant="h5" component="h2">
-            Est. Duration:
-          </Typography>
-          <Typography variant="body1" color="textSecondary" component="p">
-            1hr 45min
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Est. Duration:
+            </Typography>
+                <Typography variant="body1" color="textSecondary" component="p">
+                  1hr 45min
+            </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        {/* // )} */}
+      </Dialog>
+    </>
   );
 };
 
