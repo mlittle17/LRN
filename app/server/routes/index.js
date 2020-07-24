b3a08cd02081a27006f0f991bae20b185950e1c4
JerryMcDonald <jandh62808@gmail.com>
2020-07-23 20:55:01 -0500

    Route additions to fix student issue

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -6,7 +6,9 @@ const { eventRouter } = require('./event');
 const routes = Router();
 
 routes.use('/topic', topicRouter);
+routes.use('/student/users', usersRouter);
 routes.use('/users', usersRouter);
+routes.use('/student/event', eventRouter);
 routes.use('/event', eventRouter);
 
 module.exports = {
