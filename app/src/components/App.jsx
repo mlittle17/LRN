8afbc51658717397bc4a48730af2dc887c2f0f71
Mac <mlittle17@gmail.com>
2020-07-23 23:00:35 -0500

    adding site url to redirect

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -70,7 +70,7 @@ function App() {
   // }, []);
 
   const googleLogin = () => {
-    window.location.replace('http://localhost:8080/auth/login');
+    window.location.replace('https://lrn-solid-sun-282620.uc.r.appspot.com/');
   };
 
   const googleLogout = () => {
