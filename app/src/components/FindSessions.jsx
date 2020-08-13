3474ef7ef0f40f9273ce5d9728f0e501da2dc432
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-08-13 17:26:09 -0500

    Remove the styles array from creation directly within the Google Maps instantiation and import from the newly added js file

--- app/src/components/FindSessions.jsx
+++ app/src/components/FindSessions.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable quote-props */
 import React, { useState, useEffect, useLayoutEffect } from 'react';
-import Cleave from 'cleave.js/react';
 import Geocode from 'react-geocode';
 import { useGoogleMaps } from 'react-hook-google-maps';
+import Cleave from 'cleave.js/react';
 
 import { Button, Form } from 'semantic-ui-react';
 import { makeStyles } from '@material-ui/core/styles';
@@ -14,8 +14,10 @@ import {
 } from '@material-ui/core';
 
 import '../styles/Form.css';
+import mapStyles from '../styles/Map.js'
 
 // Set up the Geocoding for transforming the zip to lat and lon
+// process.env.GOOGLE_API_KEY
 Geocode.setApiKey('AIzaSyCVPR2bv5DCVKltpal636K0ei6zCIGb_68');
 
 const useStyles = makeStyles((theme) => ({
@@ -130,183 +132,7 @@ const FindSessions = ({ user, sessions }) => {
       streetViewControl: false,
       rotateControl: false,
       fullscreenControl: true,
-      styles: [
-        {
-          'featureType': 'all',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-            {
-              'color': '#51562d',
-            },
-          ],
-        },
-        {
-          'featureType': 'all',
-          'elementType': 'labels',
-          'stylers': [
-            {
-              'visibility': 'off',
-            },
-            {
-              'color': '#b5c75c',
-            },
-          ],
-        },
-        {
-          'featureType': 'all',
-          'elementType': 'labels.text.fill',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'all',
-          'elementType': 'labels.text.stroke',
-          'stylers': [
-            {
-              'color': '#000000',
-            },
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'administrative.land_parcel',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape',
-          'elementType': 'all',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-            {
-              // #474a00
-              'color': '#43462c',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape.man_made',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-            {
-              'color': '#c4a96f',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape.natural',
-          'elementType': 'all',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape.natural',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape.natural',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'landscape.natural.landcover',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-            {
-              'color': '#000000',
-            },
-          ],
-        },
-        {
-          'featureType': 'road.highway',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-            {
-              'color': '#9d7a22',
-            },
-          ],
-        },
-        {
-          'featureType': 'road.arterial',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'road.arterial',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'color': '#7b7a4d',
-            },
-          ],
-        },
-        {
-          'featureType': 'water',
-          'elementType': 'geometry',
-          'stylers': [
-            {
-              'visibility': 'on',
-            },
-          ],
-        },
-        {
-          'featureType': 'water',
-          'elementType': 'geometry.fill',
-          'stylers': [
-            {
-              'color': '#f6fef5',
-            },
-          ],
-        },
-      ],
+      styles: mapStyles,
     },
   );
 
