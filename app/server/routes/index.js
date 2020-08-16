dceeb483b05ce9eb4719e2d04ceb4a2ce250d2b2
neisha1618 <neisha1618@gmail.com>
2020-08-16 18:17:46 -0500

    coment ou topics

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const { topicRouter } = require('./topic');
+// const { topicRouter } = require('./topic');
 const { usersRouter } = require('./users');
 const { eventRouter } = require('./event');
 
 const routes = Router();
 
-routes.use('/topic', topicRouter);
+// routes.use('/topic', topicRouter);
 routes.use('/student/users', usersRouter);
 routes.use('/users', usersRouter);
 routes.use('/student/event', eventRouter);
