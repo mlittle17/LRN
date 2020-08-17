ebf84be945027c1c9e46ffd70942386b22b962c1
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-08-17 15:57:03 -0500

    Remove the unused and refactored code and imports from the Home page component. Pass the registered sessions collection along to the Upcming Sessions component

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useState, Route } from 'react';
+import React from 'react';
 import { Link, Switch } from 'react-router-dom';
 
-// import axios from 'axios';
-import moment, { isBefore, isSameOrAfter } from 'moment';
-
-import { Grid, Fab } from '@material-ui/core';
-import VoiceChatIcon from '@material-ui/icons/VoiceChat';
 import { Button } from 'semantic-ui-react';
+import { Grid } from '@material-ui/core';
 import ProfileCard from './ProfileCard.jsx';
 import Binder from './Binder.jsx';
 import UpcomingSessions from './UpcomingSessions.jsx';
 
 const Home = ({
-  user, binder, sessions, setNavbarSessionName,
+  user, binder, sessions, regSessions, setNavbarSessionName,
 }) => {
-  const [currentTime, setCurrentTime] = useState();
-  const [joinSession, setJoinSession] = useState(false);
-  const [sessionToPlace, setSessionToPlace] = useState({});
 
   return (
     <div className="Home">
@@ -24,13 +17,12 @@ const Home = ({
         <ProfileCard userInfo={user} />
         <div>
           <>
-            {/* <br /> */}
             <Button as={Link} to="/registered" style={{ backgroundColor: '#474a2c', color: '#f6fef5' }}>My Sessions</Button>
             <Button as={Link} to="/find" style={{ backgroundColor: '#474a2c', color: '#f6fef5' }}>Find Sessions</Button>
             <Button as={Link} to="/create" style={{ backgroundColor: '#474a2c', color: '#f6fef5' }}>Create Session</Button>
           </>
           <div style={{ marginTop: '30px' }}>
-            <UpcomingSessions sessions={sessions} user={user} setNavbarSessionName={setNavbarSessionName} />
+            <UpcomingSessions sessions={sessions} regSessions={regSessions} user={user} setNavbarSessionName={setNavbarSessionName} />
           </div>
           <Binder binder={binder} />
         </div>
