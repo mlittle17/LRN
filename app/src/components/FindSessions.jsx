2dc89dbc7114cfc682b8f0d76b69123df7546c7b
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-08-18 06:35:45 -0500

    Completely refactor the Find Sessions component. Remove the usage of the react-hook-google-maps package, replace with raw Google Maps JavaScript Api usage. Recreate the map, watch the currMapLocs state variable for change to determine where to replace the markers and to remove. REvisit and adjust the clearMarkersWhere functionality and logic. Force the search values to case to allow for error handling in the case that the user enters the subject differently when creating the form (mostly error in casing when seeding the database with capitalization). Limit the search send on the date specifically to when the user has finished typing. Employ the useRef hook to track the current instance of the map and relate it to the jsx element associated. Adjust the value retrieved from the date input, taking the formatted date with delimiters (/) rather than the raw value like perviously versioned. Replace and fromat the generic 'Scheduled Sessions' header of the dialog lists with the clicked Zip and search location icon.

--- app/src/components/FindSessions.jsx
+++ app/src/components/FindSessions.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable quote-props */
-import React, { useState, useEffect, forwardRef } from 'react';
+import React, { useState, useEffect, forwardRef, useRef } from 'react';
 import Geocode from 'react-geocode';
-import { useGoogleMaps } from 'react-hook-google-maps';
 import Cleave from 'cleave.js/react';
 
 import MapSessionList from './MapSessionList';
 import { Button, Form } from 'semantic-ui-react';
 import { makeStyles } from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
+import LocationSearchingIcon from '@material-ui/icons/LocationSearching';
 import {
   Card, CardActionArea, CardContent, Collapse, Dialog, DialogTitle, Divider,
-  Grid, IconButton, List, ListItem, Slide, Typography, Zoom,
+  Grid, IconButton, Typography, Zoom,
 } from '@material-ui/core';
 
 import '../styles/Form.css';
@@ -83,6 +83,8 @@ const subOptions = [
   { key: 'ot', text: 'Other', value: 'other' },
 ];
 
+const markers = [];
+
 const FindSessions = ({ user, sessions, regSessions }) => {
   const classes = useStyles();
   // Input field states
@@ -91,17 +93,17 @@ const FindSessions = ({ user, sessions, regSessions }) => {
   const [zip, setZip] = useState('*****');
 
   // Map and location data states
+  const ref = useRef()
+  let [map, setMap] = useState()
   const [listOpen, setListOpen] = useState(false);
   const [userLoc, setUserLoc] = useState(() => {
     if (user) {
-      console.log('user', user);
       return user.location;
     }
   });
   const [sessionColl, setSessionColl] = useState({});
-  const [sessionList, setSessionsList] = useState([]);
+  const [sessionList, setSessionList] = useState({});
   const [currMapLocs, setCurrMapLocs] = useState([]);
-  const markers = [];
   // const [markers, setMarkers] = useState([]);
 
   // The custom marker images
@@ -114,36 +116,67 @@ const FindSessions = ({ user, sessions, regSessions }) => {
 
   const onSessionSubjectChange = (e, result) => {
     const { value } = result;
+    showMarkers();
     setSubject(value);
   };
 
   const onSessionDateChange = (e) => {
-    setSessionDate(e.target.rawValue);
+    showMarkers();
+    setSessionDate(e.target.value);
   };
 
   const onZipChange = (e) => {
     setZip(e.target.rawValue);
   };
 
-  let { ref, map, google } = useGoogleMaps(
-    // Use your own API key, you can get one from Google (https://console.cloud.google.com/google/maps-apis/overview)
-    'AIzaSyCVPR2bv5DCVKltpal636K0ei6zCIGb_68',
-    {
-      center: { lat: 39.7837304, lng: -100.4458825 }, // set the map to focus over the us when it loads, so the user experiences the zoom in on their location once the user object becomes available
-      zoom: 4.4,
-      zoomControl: false,
-      mapTypeControl: true,
-      // mapTypeControlOptions: {
-      //   style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-      //   mapTypeIds: ['roadmap', 'terrain'],
-      // },
-      scaleControl: false,
-      streetViewControl: false,
-      rotateControl: false,
-      fullscreenControl: true,
-      styles: mapStyles,
-    },
-  );
+  useEffect(() => {
+    const onLoad = () => setMap(new window.google.maps.Map(ref.current, {
+          center: { lat: 39.7837304, lng: -100.4458825 }, // set the map to focus over the us when it loads, so the user experiences the zoom in on their location once the user object becomes available
+          zoom: 4.4,
+          zoomControl: false,
+          mapTypeControl: true,
+          // mapTypeControlOptions: {
+          //   style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+          //   mapTypeIds: ['roadmap', 'terrain'],
+          // },
+          scaleControl: false,
+          streetViewControl: false,
+          rotateControl: false,
+          fullscreenControl: true,
+          styles: mapStyles,
+        }
+      ))
+    if (!window.google) {
+      const script = document.createElement(`script`)
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCVPR2bv5DCVKltpal636K0ei6zCIGb_68`
+      document.head.append(script)
+      script.addEventListener(`load`, onLoad)
+      return () => script.removeEventListener(`load`, onLoad)
+    } else {
+      onLoad()
+    }
+    
+  }, []);
+
+  // let { ref, map, google } = useGoogleMaps(
+  //   // Use your own API key, you can get one from Google (https://console.cloud.google.com/google/maps-apis/overview)
+  //   'AIzaSyCVPR2bv5DCVKltpal636K0ei6zCIGb_68',
+  //   {
+  //     center: { lat: 39.7837304, lng: -100.4458825 }, // set the map to focus over the us when it loads, so the user experiences the zoom in on their location once the user object becomes available
+  //     zoom: 4.4,
+  //     zoomControl: false,
+  //     mapTypeControl: true,
+  //     // mapTypeControlOptions: {
+  //     //   style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+  //     //   mapTypeIds: ['roadmap', 'terrain'],
+  //     // },
+  //     scaleControl: false,
+  //     streetViewControl: false,
+  //     rotateControl: false,
+  //     fullscreenControl: true,
+  //     styles: mapStyles,
+  //   },
+  // );
 
   // console.log('map instance:', map); // instance of created Map object (https://developers.google.com/maps/documentation/javascript/reference/map)
   // console.log('google api object:', google); // google API object (easily get google.maps.LatLng or google.maps.Marker or any other Google Maps class)
@@ -160,20 +193,16 @@ const FindSessions = ({ user, sessions, regSessions }) => {
 
   // Removes the markers from the map, but does not delete them from the array
   const clearMarkersWhere = (searchType, searchValue) => {
-    markers.length = 0
-    
-    // markers.forEach((marker) => {
-    //   marker.setMap(null);
-    //   marker.icon = null;
-    //   marker.zip = null;
-    //   marker.setVisible(false);
-    //   console.log('marker after clear:', marker)
-      // if (!(sessionColl[marker.zip].every((session) => session[searchType] === searchValue))) {
-      //   // marker.setMap(null);
-      //   marker.setVisible(false);
+    markers.forEach((marker) => {
+      // marker.setVisible(false);
+      // sessionColl[marker.zip].forEach((session) => console.log(session[searchType], searchValue));
+      if (!(sessionColl[marker.zip].some((session) => session[searchType].toLowerCase() === searchValue))) {
+        // marker.setMap(null);
+        marker.setMap(null);
+        marker.setVisible(false);
 
-      // }
-    // });
+      }
+    });
     console.log('markers after clear:', markers)
     // if (marker[searchType] !== searchValue) {
   };
@@ -191,7 +220,7 @@ const FindSessions = ({ user, sessions, regSessions }) => {
     console.log('add marker, markerObj:', markerObj);
     const { zipcode } = markerObj;
 
-    const marker = new google.maps.Marker({
+    const marker = new window.google.maps.Marker({
       map,
       position: markerObj,
       title: zipcode.toString(),
@@ -201,7 +230,7 @@ const FindSessions = ({ user, sessions, regSessions }) => {
     });
     marker.zip = zipcode;
     marker.addListener('click', () => {
-      setSessionsList(sessionColl[marker.title]);
+      setSessionList({ zip: marker.title, sessions: sessionColl[marker.title] });
       setListOpen(true);
       // map.setZoom(8);
       // map.setCenter(marker.getPosition());
@@ -214,7 +243,7 @@ const FindSessions = ({ user, sessions, regSessions }) => {
 
   // Execute when map object is ready
   // Add the markers for session object zips that exist within the state
-  // useEffect(() => {
+  useEffect(() => {
 
     if (map) {
     //   if (user) {
@@ -226,11 +255,12 @@ const FindSessions = ({ user, sessions, regSessions }) => {
     //   }
     
     // Add markers to all appropriate zips
+    console.log('currMapLocs', currMapLocs);
     currMapLocs.forEach((mapLoc) => {
         addMarker(mapLoc, sessionsMarker);
       });
     }
-  // }, [map])
+  }, [map, currMapLocs])
 
   // Watching the sessions prop for update
   useEffect(() => {
@@ -248,12 +278,15 @@ const FindSessions = ({ user, sessions, regSessions }) => {
     delete zipsForMarkers.null;
     setSessionColl(zipsForMarkers);
 
+    // Iterate over the new zipcode key sorted collection of sessions arrays.
+    // Geocode the zipcodes associated with the sessions in order to create the collection that can be used for populating the map with markers
     Object.keys(zipsForMarkers).forEach((zipForMarker) => {
       console.log(`zipForMarker: ${zipForMarker}: ${zipsForMarkers[zipForMarker]}`);
-      Geocode.fromAddress(zipForMarker).then(
-        response => {
+      Geocode.fromAddress(zipForMarker)
+      .then((response) => {
           const { lat, lng } = response.results[0].geometry.location;
-          if (currMapLocs.every((mapObj) => mapObj.zipcode !== zipForMarker)) {
+          if (currMapLocs.every((mapObj) => mapObj.zipcode.toString() !== zipForMarker)) {
+            // Build the collection in simulated state as the session objects are transformed into the usable lat/lng format
             setCurrMapLocs([...currMapLocs, { lat, lng, zipcode: zipForMarker }]);
           }
         },
@@ -289,7 +322,9 @@ const FindSessions = ({ user, sessions, regSessions }) => {
   // (When a user searches by date)
   useEffect(() => {
     // Remove any markers from the map that do not have any sessions that have the subject of searched value
-    clearMarkersWhere('date', sessionDate);
+    if (sessionDate.toString().length === 10) {
+      clearMarkersWhere('date', sessionDate);
+    }
   }, [sessionDate]);
 
   // Watching the zip state value for update
@@ -404,15 +439,15 @@ const FindSessions = ({ user, sessions, regSessions }) => {
         >
           <Card className={classes.card} variant="outlined">
             <DialogTitle id="max-width-dialog-title" className={classes.actionArea}>
-              <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close" style={{ float: 'left' }}>
+              <IconButton edge="start" color="inherit" fontSize="large" aria-label="close" style={{ float: 'left' }} onClick={handleClose} >
                 <CloseIcon />
               </IconButton>
               <Typography variant="h3" align="center">
-                SCHEDULED SESSIONS
-                </Typography>
+                <LocationSearchingIcon fontSize="large" /><b>SESSIONS: {sessionList.zip}</b>
+              </Typography>
             </DialogTitle>
 
-            <MapSessionList user={user} sessionList={sessionList} regSessions={regSessions}/>
+            <MapSessionList user={user} sessionList={sessionList.sessions} sessionsZip={sessionList.zip} regSessions={regSessions}/>
           </Card>
         </Dialog>
 
