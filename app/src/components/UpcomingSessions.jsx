b942497e931058f01ec2daf4ae8ab6b21b33b1f1
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-08-18 11:40:33 -0500

    Add conditional rendering of call to action Typography tage for when the user is new and has not yet registered for any sessions

--- app/src/components/UpcomingSessions.jsx
+++ app/src/components/UpcomingSessions.jsx
@@ -55,6 +55,11 @@ const UpcomingSessions = ({ user, sessions, regSessions, setNavbarSessionName })
   return (
     <div>
       <Typography gutterBottom variant="h4" component="h6" style={{ color: '#2d2e2e' }}><b>UPCOMING SESSIONS</b></Typography>
+      {upcomingSessions.length < 1
+        ? (
+          <Typography variant="h6" component="h6" style={{ color: '#a58e57' }}><b>Try registering for some sessions!</b></Typography>
+        )
+        : (
       <Slider {...settings} style={{ width: '600px', height: '300px', backgroundColor: '#e0e1e2' }}>
         {rows.map((sessionPage) => {
           return (
@@ -64,6 +69,8 @@ const UpcomingSessions = ({ user, sessions, regSessions, setNavbarSessionName })
           );
         })}
       </Slider>
+        )
+      }
     </div>
   );
 };
